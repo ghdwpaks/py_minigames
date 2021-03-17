23cba68103e3300c433dcc2401ee62de1d1ddaf3
ghdwpaks <hjm7904@gmail.com>
2021-03-17 22:52:05 +0900

    made func color in the maze '미로 내 색칠 영역 지정 구현'

---
+++ GUI/day07/ex23_make_func_coloring.py
@@ -0,0 +1,57 @@
+import tkinter as t
+
+key = ""
+def key_down(e) :
+    global key
+    key = e.keysym
+def key_up(e) :
+    global key
+    key = ""
+
+mx = 1
+my = 1
+def main_proc() :
+    global mx,my
+    if key == "Up" and maze[my - 1][mx] == 0 :
+        my = my - 1
+    if key == "Down" and maze[my + 1][mx] == 0:
+        my = my + 1
+    if key == "Left" and maze[my][mx - 1] == 0 :
+        mx = mx - 1
+    if key == "Right" and maze[my][mx + 1] == 0 :
+        mx = mx + 1
+    if maze[my][mx] == 0:
+        maze[my][mx] = 2
+        c.create_rectangle(mx*80,my*80,mx*80+79,my*80+79,fill="pink",width=0)
+    c.delete("MYCHR")
+    c.create_image(mx*80+40,my*80+40,image=img01,tag="MYCHR")
+    #c.coords("MYCHR",mx*80+40,my*80+40)
+
+    r.after(300,main_proc)
+    
+r = t.Tk()
+r.title("미로 안 이동하기")
+r.bind("<KeyPress>",key_down)
+r.bind("<KeyRelease>",key_up)
+c = t.Canvas(width=800,height=560,bg="skyblue")
+c.pack()
+maze = [
+[1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,1,0,0,1],
+[1,0,1,1,0,0,1,0,0,1],
+[1,0,0,1,0,0,0,0,0,1],
+[1,0,0,1,1,1,1,1,0,1],
+[1,0,0,0,0,0,0,0,0,1],
+[1,1,1,1,1,1,1,1,1,1]
+]
+
+for y in range(7) :
+    for x in range(10) :
+        if maze[y][x] == 1:
+            c.create_rectangle(x*80,y*80,x*80+79,y*80+79,fill="gray",width=0)
+img01 = t.PhotoImage(file="hk3.png")
+c.create_image(mx * 80 + 40, my*80+40,image=img01,tag="MYCHR")
+
+main_proc()
+r.mainloop()
+
