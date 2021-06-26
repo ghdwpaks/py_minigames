57879c2e3acaada5d8bab8956a2651bd8e476b1b
ghdwpaks <hjm7904@gmail.com>
2021-06-26 22:52:22 +0900

    check full house 함수 세작 및 check small straight 설계

--- yatch/main.py
+++ yatch/main.py
@@ -32,8 +32,9 @@ def get_dice_data() :
     return choose_count
 
 def check_able(dice) :
+    dice.sort()
     #dice = [1,1,1,3,5]
-    possible_section = [1,1,1,1,1,1,1,check_4_of_a_kind()]
+    possible_section = [check_4_of_a_kind()]
     pass
     
 
@@ -48,24 +49,55 @@ def check_4_of_a_kind(dice) :
             return 1
     return 0
             
+def check_full_house(dice) :
+
+    #dice = [1,1,1,3,5]
+    
+    for i in range(len(dice)) :
+        base_num = dice[i]
+        stack = 0
+        for j in range(len(dice)) :
+            if dice[j] == base_num :
+                stack += 1
+        if stack == 2 :
+            for l in range(len(dice)) :
+                base_num2 = dice[l]
+                stack2 = 0
+                for k in range(len(dice)) :
+                    if dice[k] == base_num2 :
+                        stack2 += 1
+                if stack2 == 3 :
+                    return 1
+    return 0
+
+def check_small_straight(dice) :
+    #dice = [1,1,1,3,5]
+    for i in range(len(dice)) :
+        for j in range(i) :
+            stack = 0
+            if dice[i] == dice[i-j] :
+                stack += 1
+            if stack >= 4 :
+                return 1
+    return 0
 
 
-def print_can_set_score(possible_section = [0,0,0,0,0,0,0,0,0,0,0,0]) :
+
+        
+
+
+
+
+
+def print_can_set_score(possible_section = [0,0,0,0,0]) :
     #possible_section = [1,1,1,0,1,1,0,0,1,1,1,1]
     '''
     Possible_section
-    0 : Aces    1이 나온 주사위 눈의 총합
-    1 : Deuces  2이 나온 주사위 눈의 총합
-    2 : Threes  3이 나온 주사위 눈의 총합
-    3 : Fours   4가 나온 주사위 눈의 총합
-    4 : Fives   5가 나온 주사위 눈의 총합
-    5 : Sixes   6이 나온 주사위 눈의 총합
-    6 : Choice          주사위 눈 5개의 총합. 최대 30점
-    7 : 4 of a Kind     동일한 주사위 눈이 4개 이상일때, 주사위 눈 5개의 총합. 최대 30점
-    8 : Full House      동일한 주사위 눈이 각각 3개, 2개일때, 주사위 눈 5개의 총합. 최대 30점
-    9 : Small Straight  이어지는 주사위 눈이 4개 이상일때, 고정 15점
-    10 : Large Stringt  이어지는 주사위 눈이 5개일때. 고정 30점
-    11 : Yacht          동일한 주사위 눈이 5개일때. 고정 50점
+    0 : 4 of a Kind     동일한 주사위 눈이 4개 이상일때, 주사위 눈 5개의 총합. 최대 30점
+    1 : Full House      동일한 주사위 눈이 각각 3개, 2개일때, 주사위 눈 5개의 총합. 최대 30점
+    2 : Small Straight  이어지는 주사위 눈이 4개 이상일때, 고정 15점
+    3 : Large Stringt  이어지는 주사위 눈이 5개일때. 고정 30점
+    4 : Yacht          동일한 주사위 눈이 5개일때. 고정 50점
     '''
     prints = ["Aces\t\t1이 나온 주사위 눈의 총합","Deuces\t\t2이 나온 주사위 눈의 총합","Threes\t\t3이 나온 주사위 눈의 총합","Fours\t\t4가 나온 주사위 눈의 총합","Fives\t\t5가 나온 주사위 눈의 총합","Sixes\t\t6이 나온 주사위 눈의 총합","Choice\t\t주사위 눈 5개의 총합. 최대 30점","4 of a Kind\t동일한 주사위 눈이 4개 이상일때, 주사위 눈 5개의 총합. 최대 30점","Full House\t동일한 주사위 눈이 각각 3개, 2개일때, 주사위 눈 5개의 총합. 최대 30점","Small Straight\t이어지는 주사위 눈이 4개 이상일때, 고정 15점","Large Stringt\t이어지는 주사위 눈이 5개일때. 고정 30점","Yacht\t\t동일한 주사위 눈이 5개일때. 고정 50점"]
     
@@ -76,10 +108,14 @@ def print_can_set_score(possible_section = [0,0,0,0,0,0,0,0,0,0,0,0]) :
     print()
     
 round = 12
+print(check_small_straight([1,2,3,4,4]))
 
+
+'''
 print_can_set_score()
 for i in range(round) :
     dice_result = get_dice_data()
+'''
     
 
 
